0fed3417c8e2edad0dfcd4c468f2357b600f3e39
haides002 <me>
2024-09-01 20:49:55 +0200

    adding the things i made allready

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+struct ImageData {
+    height: u32,
+    width: u32,
+    data: Vec<[u8;3]>,
+}
+
+fn main() {
+    let image = read_image("/home/linus/Dev/rust/pixel_sort/test_image.jpg").unwrap();
+    write_image("/home/linus/Dev/rust/pixel_sort/output.jpg", image);
+}
+
+fn write_image(file_path: &str, image_data: ImageData) {
+    let mut image_buffer = image::RgbImage::new(image_data.width, image_data.height);
+
+}
+
+
+
+
+fn read_image(file_path: &str) -> Result<ImageData, image::ImageError> {
+    // i now haz a image_buffer
+    let binding = image::ImageReader::open(file_path)?.decode()?;
+    let image_buffer = binding.as_rgb8().expect("Failed to convert image to rgb8 format!");
+    
+    // create the image data struct & give it width and height
+    let mut image_data = ImageData {
+        height: image_buffer.height(),
+        width: image_buffer.width(),
+        data: vec![]
+    };
+
+    // fill the data vector with pixel RGB values
+    {
+        let mut y:u32 = 0;
+        while y < image_data.height {
+            let mut x:u32 = 0;
+            while x < image_data.width {
+                image_data.data.push(image_buffer.get_pixel(x,y).0);
+                x = x + 1;
+            }
+            y = y + 1;
+        }
+    }
+
+    return Ok(image_data);
+}

1d9faf3749208daa8471018b6c70a87c951748c3
haides002 <105516284+haides002@users.noreply.github.com>
2024-09-08 16:14:21 +0200

    created write image function & moved testing materials to a seperate dir wich is ignored

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,30 @@ fn main() {
 
 // ======== Writing the edited Image ======== //
 fn write_image(file_path: &str, image_data: ImageData) {
-    let mut image_buffer = image::RgbImage::new(image_data.width, image_data.height);
+    // create an imagebuffer with the size of our raw data
+    let mut image_buffer = image::DynamicImage::new(image_data.width, image_data.height, image::ColorType::Rgb8);
 
+    // write into the image buffer from the raw data
+    {
+        let mut y:u32 = 0;
+        let mut id:usize = 0;
+        while y < image_data.height {
+            let mut x:u32 = 0;
+            while x < image_data.width {
+                let pixel = image::Rgb{
+                    0: image_data.data[id],
+                };
+
+                image_buffer.as_mut_rgb8().unwrap().put_pixel(x, y, pixel);
+                x = x + 1;
+                id = id + 1;
+            }
+            y = y +1;
+        }
+    }
+
+    // write the image buffer to disk
+    image_buffer.save(file_path).unwrap();
 }
 
 
